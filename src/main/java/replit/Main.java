ecf0e3e2b5d777aaeb3f31f8eff5edbde2f217c8
Carlos Jimenez <cejtok@gmail.com>
2021-09-13 22:41:40 +0000

    Todo listo!

--- src/main/java/replit/Main.java
+++ src/main/java/replit/Main.java
@@ -8,6 +8,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.MenuBar;
@@ -27,64 +29,64 @@ public class Main extends Application
     VBox vbox;
     Scene scene;
 
-    Menu menu = new Menu("Menu 1");;
+    Menu menu = new Menu("Archivo");
     MenuItem menuItem = new MenuItem("Cargar desde bytecode");
+    MenuItem menuItem2 = new MenuItem("Cerrar");
 
     menu.getItems().add(menuItem);
+    menu.getItems().add(menuItem2);
     MenuBar menuBar = new MenuBar();
-    menuBar.getMenus().add(menu);
-
-    
-    tf = new TextField("Text Field!");
-    tf.setMaxWidth(200);
-
-    label = new Label("Type text and click the button");
-    button = new Button("Click");
-
-    Button buttonAgregar = new Button("Agregar valla");
+    menuBar.getMenus().add(menu);        
 
     Label label1 = new Label("Ancho");
     Label label2 = new Label("Alto");
     Label label3 = new Label("En Uso");
     Label label4 = new Label("Empresa");
 
+    VBox vbox1 = new VBox(label1,label2,label3,label4);
+    vbox1.setSpacing(10);
+
     TextField tf1 = new TextField("Ancho");
     TextField tf2 = new TextField("Alto");
-    TextField tf3 = new TextField("En uso");
+    //tf2.setMaxWidth(200);
+    ComboBox combobox = new ComboBox();
+    combobox.getItems().add("Si");
+    combobox.getItems().add("No");
+    combobox.setValue("Si");
     TextField tf4 = new TextField("Empresa");
 
+    Button buttonAgregar = new Button("Agregar valla");     
 
-    HBox hbox1 = new HBox(label1, tf1);
-    HBox hbox2 = new HBox(label2, tf2);
-    HBox hbox3 = new HBox(label3, tf3);
-    HBox hbox4 = new HBox(label4, tf4);
-
-    
-    hbox1.setAlignment(Pos.CENTER_RIGHT);
-    hbox2.setAlignment(Pos.CENTER_RIGHT);
-    hbox3.setAlignment(Pos.CENTER_RIGHT);
-    hbox4.setAlignment(Pos.CENTER_RIGHT);
-
-     VBox vbox1 = new VBox(label1,label2,label3,label4);
-     vbox1.setSpacing(10);
-
-    VBox vbox2 = new VBox(tf1,tf2,tf3,tf4,buttonAgregar);
+    VBox vbox2 = new VBox(tf1,tf2,combobox,tf4,buttonAgregar);
     VBox vbox3 = new VBox();
 
-    HBox hbox6 = new HBox(vbox1,vbox2,vbox3);
+    HBox hbox = new HBox(vbox1, vbox2, vbox3);
 
     vbox2.setPrefWidth(140);
-
-
  
 
-    button.setOnAction(new EventHandler<ActionEvent>() {
+    buttonAgregar.setOnAction(new EventHandler<ActionEvent>() {
       @Override public void handle(ActionEvent e) {
-        label.setText(tf.getText());
+        label1.setText(tf1.getText());
       }
     });
 
-    vbox = new VBox(menuBar, hbox6, button);//label, tf,
+    menuItem.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent e) {
+        label1.setText("Carlos");
+      }
+    });
+
+    menuItem2.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent e) {
+        label1.setText("Cerrar");
+      }
+    });
+
+
+    TextArea textarea = new TextArea();
+
+    vbox = new VBox(menuBar, hbox, textarea);
     vbox.setSpacing(0);
     //vbox.setAlignment(Pos.CENTER);
     scene = new Scene(vbox, 300, 200);
